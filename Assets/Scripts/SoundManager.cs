d8121356833e59f191f7a9f2bd0422375485991b
DuckS0ul <qq13732291063@gmail.com>
2024-01-02 04:09:36 +0000

    1

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; set; }
+
+
+    //Sound FX
+    public AudioSource dropItemSound;
+
+    public AudioSource pickUpItemSound;
+    public AudioSource toolSwingSound;
+    public AudioSource craftingSound;
+    public AudioSource chopSound;
+
+    public AudioSource grassWalkSound;
+
+    //Music
+    public AudioSource startingZoneBGMusic;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+
+    public void PlaySound(AudioSource soundToPlay)
+    {
+        if (!soundToPlay.isPlaying)
+        {
+            soundToPlay.Play();
+        }
+    }
+
+    
+
+
+
+
+}
